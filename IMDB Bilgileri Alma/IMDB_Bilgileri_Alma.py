be963249fb2ae122890cc92c176cbdcc7d119c6e
Yusuf Can <48151439+WhiteRedTHT@users.noreply.github.com>
2019-11-07 18:35:09 +0300

    Add files via upload

---
+++ IMDB Bilgileri Alma/IMDB_Bilgileri_Alma.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
+
+html = requests.get(url).content
+soup = BeautifulSoup(html, "html.parser")
+list = soup.find("tbody", {"class":"lister-list"}).find_all("tr")
+
+count = 0
+for tr in list:
+    title = tr.find("td", {"class":"titleColumn"}).find("a").text
+    year = tr.find("td",{"class":"titleColumn"}).find("span").text.strip("()")
+    rating = tr.find("td",{"class":"ratingColumn imdbRating"}).find("strong").text
+    count+=1
+    print(f"{count}- Filmin İsmi: {title.ljust(100)} Filmin Yılı: {year} Filmin Puanı: {rating}")
+
+
+
+
+
